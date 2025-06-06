0484c85e454debf90071d49f71147ca8a942ef1f
Bolo Chavez <bchavez@ixsystems.com>
2025-06-06 15:50:58 -0400

    adding funcitonality

--- seend.c
+++ seend.c
@@ -17,6 +17,68 @@ void skill(int sig){
     run = 0;
 }
 
+
+
+double copy_file_chunk(int r_file, int w_file, int page_size){
+    size_t ret;
+    long long elapsed;
+    double elapsed_sec;
+    void * buffer;
+
+    if(posix_memalign(&buffer, page_size, page_size)){
+   	perror("posix_memalign");
+	return 1;
+    }
+
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    while((ret = read(r_file, buffer, page_size )) > 0){
+        write(w_file, buffer, ret);
+    }
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    elapsed  = (end.tv_sec - start.tv_sec) * 1000000000LL + 
+                       (end.tv_nsec - start.tv_nsec);
+    free(buffer);
+    return elapsed;
+
+}
+
+double copy_file_sendfile(int r_file, int w_file, off_t size){
+    long long elapsed;
+    double elapsed_sec;
+
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    sendfile(w_file, r_file, NULL, size);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    elapsed  = (end.tv_sec - start.tv_sec) * 1000000000LL + 
+                       (end.tv_nsec - start.tv_nsec);
+    return elapsed;
+}
+
+void print_stats(off_t size, long long elapsed){
+    double elapsed_sec = elapsed / 1000000000.0;
+    long long total_bytes = (long long) (size);
+    double bandwidth = total_bytes / elapsed_sec;
+    printf("\n\n\n elapsed: %ld s", elapsed);
+    printf("\n bw: %lf KiB/s", bandwidth/ 1024);
+    printf("\n bw: %lf MiB/s", bandwidth / (1024*1024));
+    printf("\n bw: %lf GiB/s\n", bandwidth / (1024 * 1024 * 1024));
+
+
+}
+
+
+void drop_caches(){
+    sync();
+    int dc_file = open("/proc/sys/vm/drop_caches", O_WRONLY);
+    if (dc_file >= 0){
+        write(dc_file, "3", 1);
+    } else {
+        perror("open");
+
+    }
+    close(dc_file);
+}
+
 int main(int argc, char *argv[]){
     signal(SIGINT, skill);
     signal(SIGTERM, skill);
@@ -24,15 +86,10 @@ int main(int argc, char *argv[]){
     int open_flags  = O_RDONLY;
     int write_flags  = O_WRONLY | O_CREAT;
     int page_size  = getpagesize();
-    int intervals = 10;
-    int interval_time = 2;
-    long long total_bytes;
-    double bandwidth;
     int r_file;
     int w_file;
     off_t size;
     long long elapsed;
-    int count = 0;
     size_t ret;
     int opt;
     double elapsed_sec;
@@ -60,26 +117,42 @@ int main(int argc, char *argv[]){
     }
 
     size = lseek(r_file,0, SEEK_END);
-
-
-
     lseek(r_file, 0, SEEK_SET);
     printf("\nfile size:%ld bytes \n\n", size);
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    sendfile(w_file, r_file, NULL, size);
-    clock_gettime(CLOCK_MONOTONIC, &end);
-    elapsed  = (end.tv_sec - start.tv_sec) * 1000000000LL + 
-                       (end.tv_nsec - start.tv_nsec);
-    elapsed_sec = elapsed / 1000000000.0;
-    total_bytes = (long long) (size);
-    bandwidth = total_bytes / elapsed_sec;
-    printf("\r bw: %lf MiB/s", bandwidth / (1024 * 1024));
-    fflush(stdout);
-    count = 0;
-    printf("\n\n\n elapsed: %ld s", elapsed);
-    printf("\n bw: %lf KiB/s", bandwidth/ 1024);
-    printf("\n bw: %lf MiB/s", bandwidth / (1024*1024));
-    printf("\n bw: %lf GiB/s\n", bandwidth / (1024 * 1024 * 1024));
+
+
+    printf("\nuser space stats\n", size);
+    elapsed = copy_file_chunk(r_file, w_file, page_size);
+    print_stats(size, elapsed);
+
+    lseek(r_file, 0, SEEK_SET);
+    lseek(w_file, 0, SEEK_SET);
+
+    printf("\nsend stats\n", size);
+    elapsed = copy_file_sendfile(r_file, w_file, size);
+    print_stats(size, elapsed);
+
+    lseek(r_file, 0, SEEK_SET);
+    lseek(w_file, 0, SEEK_SET);
+
+
+
+    drop_caches();
+
+    printf("\nno cache send stats\n", size);
+    elapsed = copy_file_sendfile(r_file, w_file, size);
+    print_stats(size, elapsed);
+
+    lseek(r_file, 0, SEEK_SET);
+    lseek(w_file, 0, SEEK_SET);
+
+
+    drop_caches();
+    printf("\nno cache user space stats\n", size);
+    elapsed = copy_file_chunk(r_file, w_file, page_size);
+    print_stats(size, elapsed);
+
+
     close(r_file);
     close(w_file);
 
